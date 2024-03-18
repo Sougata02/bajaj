39eb94bbb0be40f7fdf775287b135383610b59e3
sougata2002 <sougatabhowmick62@gmail.com>
2024-03-18 11:46:46 +0530

    first commit 2

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+app.get('/',(req,res)=>{
+  res.send("Hello");
+})
+app.post("/bfhl", async (req, res) => {
+  const { arr } = req.body;
+  let char = [];
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i] >= "A" && arr[i] <= "Z") || (arr[i] >= "a" && arr[i] <= "z")) {
+      char.push(arr[i]);
+    }
+  }
+  for (let i = 0; i < char.length; i++) {
+    if (char[i] === char[i].toLowerCase()) {
+      char[i] = char[i].toUpperCase();
+    }
+  }
+  let odd = [];
+  let even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      !((arr[i] >= "a" && arr[i] <= "z") || (arr[i] >= "A" && arr[i] <= "Z"))
+    ) {
+      if (i % 2 == 0) {
+        even.push(arr[i]);
+      } else {
+        odd.push(arr[i]);
+      }
+    }
+  }
+  const options = {
+    is_success: true,
+    user_id: "sougata_2110991381",
+    email: "sougata1381.be21@chitkara.edu.in",
+    roll_number: "2110991381",
+    odd_numbers: odd,
+    even_numbers: even,
+    alphabets: char,
+  };
+  res.send(options);
+});
+let port = process.env.PORT;
+app.listen(3000,()=>{
+  console.log("Server started");
+})
